e21761e7d79a2fbcf8dd2e298065ed9c1fb475e7
Janga Tushita Sharva <99260507+TushitaSharva@users.noreply.github.com>
2022-04-13 09:13:23 +0530

    Add files via upload

---
+++ ASSIGNMENT_2/codes/Assign_2.py
@@ -0,0 +1,11 @@
+#NAME OF THE STUDENT: JANGA TUSHITA SHARVA
+# ROLL NUMBER: CS21BTECH11022
+# NAME OF THE INSTRUCTOR: PROFESSOR BVV SHARMA
+# ASSIGNMENT 2, PROBABILITY AND RANDOM VARIABLES
+
+from sympy import *
+
+init_printing(use_unicode = False, wrap_line = False)
+x = Symbol('x')
+
+integrate((x**3 + 5*x**2+ 4*x + 1)/(x**2), x)
