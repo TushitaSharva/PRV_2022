fe2adcf70d02c044c1e7fbb0bcd4b7ed3e4f5c34
Janga Tushita Sharva <99260507+TushitaSharva@users.noreply.github.com>
2022-07-01 15:35:17 +0530

    Create Src.h

---
+++ Manual/Codes/Src.h
@@ -0,0 +1,90 @@
+//begin declaration
+void uniform(char *str,int len);
+double mean(char *str);
+double meansquare(char *str);
+void gaussian(char *str, int len);
+//end declaration
+
+//uniform func generator
+void uniform(char *str, int len)
+{
+int i;
+FILE *fp;
+
+fp = fopen(str,"w");
+//Generate numbers
+for (i = 0; i < len; i++)
+{
+fprintf(fp,"%lf\n",(double)rand()/RAND_MAX);
+}
+fclose(fp);
+
+}
+
+//mean calculator function
+double mean(char *str)
+{
+int i=0,c;
+FILE *fp;
+double x, temp=0.0;
+
+fp = fopen(str,"r");
+//get numbers from file
+while(fscanf(fp,"%lf",&x)!=EOF)
+{
+//Count numbers in file
+i=i+1;
+//Add all numbers in file
+temp = temp+x;
+}
+fclose(fp);
+temp = temp/(i-1);
+return temp;
+
+}
+
+//function to calculate mean of squared numbers
+
+double meansquare(char *str)
+{
+int i=0,c;
+FILE *fp;
+double x, temp=0.0;
+
+fp = fopen(str,"r");
+//get numbers from file
+while(fscanf(fp,"%lf",&x)!=EOF)
+{
+//Count numbers in file
+i=i+1;
+//Squaring and adding numbers in file
+temp = temp+x*x;
+}
+fclose(fp);
+temp = temp/(i-1);
+return temp;
+}
+
+//guassian ditribution generator
+
+void gaussian(char *str, int len)
+{
+int i,j;
+double temp;
+FILE *fp;
+
+fp = fopen(str,"w");
+//Generate numbers
+for (i = 0; i < len; i++)
+{
+temp = 0;
+for (j = 0; j < 12; j++)
+{
+temp += (double)rand()/RAND_MAX;
+}
+temp-=6;
+fprintf(fp,"%lf\n",temp);
+}
+fclose(fp);
+
+}
