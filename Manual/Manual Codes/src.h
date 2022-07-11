882689a7bf1b9730d20dcf65c9d176f3b89219d0
Janga Tushita Sharva <99260507+TushitaSharva@users.noreply.github.com>
2022-07-11 12:44:15 +0530

    Update src.h

--- Manual/Manual Codes/src.h
+++ Manual/Manual Codes/src.h
@@ -88,3 +88,26 @@ fprintf(fp,"%lf\n",temp);
 fclose(fp);
 
 }
+
+void triDist(char *tri, int len)
+{
+    int i;
+    double x, y;
+    FILE *fp1, *fp2, *destinyfile;
+    // get two uniform distributions
+    uniform("uni1.dat", len);
+    uniform("uni2.dat", len);
+    // open the two files
+    fp1 = fopen("uni1.dat", "r");
+    fp2 = fopen("uni2.dat", "r");
+    destinyfile = fopen(tri, "w");
+    // Generate numbers
+    while (fscanf(fp1, "%lf", &x) != EOF)
+    {
+        fscanf(fp2, "%lf", &y);
+        fprintf(destinyfile, "%lf\n", (double)(x + y));
+    }
+    fclose(fp1);
+    fclose(fp2);
+    fclose(destinyfile);
+}
